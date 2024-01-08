3af0ee96549e8f21f840ae94375e8e58a9b4a312
kleber chavez <kleberenriqueos@gmail.com>
2024-01-08 00:26:20 -0500

    deber estructuras

---
+++ Segundo_Parcial/Metodo_QuickSort/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Metodo_QuickSort
+{
+	internal class Program
+	{
+		static void QuickSort(int[] array, int low, int high)
+		{
+			if (low < high)
+			{
+				int partitionIndex = Partition(array, low, high);
+
+				QuickSort(array, low, partitionIndex - 1);
+				QuickSort(array, partitionIndex + 1, high);
+			}
+		}
+
+		static int Partition(int[] array, int low, int high)
+		{
+			int pivot = array[high];
+			int i = low - 1;
+
+			for (int j = low; j < high; j++)
+			{
+				if (array[j] < pivot)
+				{
+					i++;
+					Swap(array, i, j);
+				}
+			}
+
+			Swap(array, i + 1, high);
+			return i + 1;
+		}
+
+		static void Swap(int[] array, int i, int j)
+		{
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+
+		static void PrintArray(int[] array)
+		{
+			foreach (var element in array)
+			{
+				Console.Write(element + " ");
+			}
+			Console.WriteLine();
+		}
+		static void Main(string[] args)
+		{
+			int[] array = { 5, 2, 9, 1, 5, 6 };
+
+			Console.WriteLine("Array original:");
+			PrintArray(array);
+
+			QuickSort(array, 0, array.Length - 1);
+
+			Console.WriteLine("\nArray ordenado:");
+			PrintArray(array);
+		}
+	}
+}
---
+++ Segundo_Parcial/metodo_busqueda_secuencial/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace metodo_busqueda_secuencial
+{
+	internal class Program
+	{
+		static int SequentialSearch(int[] array, int target)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] == target)
+					return i;
+			}
+
+			return -1;
+		}
+
+		static void PrintArray(int[] array)
+		{
+			foreach (var element in array)
+			{
+				Console.Write(element + " ");
+			}
+			Console.WriteLine();
+		}
+		static void Main(string[] args)
+		{
+			int[] array = { 5, 2, 9, 1, 5, 6 };
+			int target = 1;
+
+			Console.WriteLine("Array:");
+			PrintArray(array);
+
+			int index = SequentialSearch(array, target);
+
+			if (index != -1)
+				Console.WriteLine($"\nEl elemento {target} se encuentra en la posición {index}.");
+			else
+				Console.WriteLine($"\nEl elemento {target} no se encuentra en el array.");
+		}
+	}
+}
